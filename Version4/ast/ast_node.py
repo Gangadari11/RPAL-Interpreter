ed7df68a42d6a5a9ffd4b3e803c00ed7eb524502
Sandali Jayawardhana <150525955+Sandali0726@users.noreply.github.com>
2025-04-23 15:42:05 +0530

    Enhance Node class with standardization logic for 'let' constructs in AST

--- Version4/ast/ast_node.py
+++ Version4/ast/ast_node.py
@@ -23,3 +23,55 @@ class Node:
     def get_degree(self):
         """Get the degree (number of chldren) of the node."""
         return len(self.children)
+
+    def get_children(self):
+        """Get the children of the node."""
+        return self.children
+    
+    def set_depth(self,depth):
+        """Set the depth of the node."""
+        self.depth = depth
+
+    def get_depth(self):
+        """Get the depth of the node."""
+        return self.depth
+
+    def set_parent(self,parent):
+        """Set the parent of the node."""
+        self.parent = parent
+
+    def get_parent(self):
+        """Get the parent of the node."""
+        return self.parent 
+
+    def standardize(self):
+        """
+        Standarize the node and its children into a core form used for evaluation.
+        This transforms constructs into a more uniform and functional structure.
+        """
+        if not self.is_standardized:
+            for children in self.children:
+                child.standardize()
+
+            # Transformation rules for various language constructs
+            if self.data == "let":
+                #letx = E in P -> gamma(lamda X. P, E)
+                # Standardize LET node
+                #       LET              GAMMA
+                #     /     \           /     \
+                #    EQUAL   P   ->   LAMBDA   E
+                #   /   \             /    \
+                #  X     E           X      P 
+                temp1 = self.children[0].children[1]
+                temp1.set_parent(self)
+                temp1.set_depth(self.depth + 1)
+                temp2 = self.children[1]
+                temp2.set_parent(self.children[0])
+                temp2.set_depth(self.depth + 2)
+                self.children[1] = temp1
+                self.children[0].set_data("lambda")
+                self.children[0].children[1] = temp2
+                self.set_data("gamma")
+
+            elif self.data == "where":
+                # 
