4fa85207272397f1afd7c27847c33a0c8bd5d058
Sandali Jayawardhana <150525955+Sandali0726@users.noreply.github.com>
2025-04-27 07:33:11 +0530

    Refactor AST and ASTFactory classes for improved readability and consistency

--- Version4/ast/ast_printer.py
+++ Version4/ast/ast_printer.py
@@ -1,4 +1,4 @@
-""" 
+"""
 Abstract Syntax Tree (AST) for RPAL Language
 Represents the structure of an RPAL program.
 """
@@ -7,21 +7,21 @@ from ast.ast_node import Node, NodeFactory
 
 class AST:
     """Class representing an Abstract Syntax Tree."""
-    def __init__(self,root=None):
+    def __init__(self, root=None):
         self.root = root
 
     def set_root(self, root):
-        """Set the rootnode of the AST."""
+        """Set the root node of the AST."""
         self.root = root
 
     def get_root(self):
         """Get the root node of the AST."""
         return self.root
-    
-    def standarize(self):
-        """Standarize the AST."""
-        if not self.root.is_standarized:
-            self.root.standarize()
+
+    def standardize(self):
+        """Standardize the AST."""
+        if not self.root.is_standardized:
+            self.root.standardize()
 
     def pre_order_traverse(self, node, i):
         """
@@ -29,11 +29,9 @@ class AST:
         
         Args:
             node: The current node
-            i: The current imdentation level
-            """
-        
+            i: The current indentation level
+        """
         print("." * i + str(node.get_data()))
-
         for child in node.children:
             self.pre_order_traverse(child, i + 1)
 
@@ -46,39 +44,39 @@ class ASTFactory:
     def __init__(self):
         pass
 
-    def get_abstract_syntax_tree(self,data):
+    def get_abstract_syntax_tree(self, data):
         """
-        Creating an AST from a string representation.
+        Create an AST from a string representation.
         
         Args:
             data: The string representation of the AST
             
         Returns:
-            AST: The creating AST
+            AST: The created AST
         """
-        root = NodeFactory.get_node(data[0],0)
-        previous_node = root
-        current_depth = 0
+        root = NodeFactory.get_node(data[0], 0)  # Create the root node
+        previous_node = root  # Initialize the previous node as the root
+        current_depth = 0  # Initialize the current depth as 0
 
         for s in data[1:]:
-            i = 0
-            d = 0
+            i = 0  # index of word
+            d = 0  # depth of node
 
             while s[i] == '.':
                 d += 1
                 i += 1
 
-            current_node = NodeFactory.get_node(s[i:], d)
+            current_node = NodeFactory.get_node(s[i:], d)  # Create the current node
 
             if current_depth < d:
-                previous_node.children.append(current_node)
-                current_node.set_parent(previous_node)
+                previous_node.children.append(current_node)  # Add current node as a child of previous node
+                current_node.set_parent(previous_node)  
             else:
-                while previous_node.get_depth() !=d :
-                    previous_node = previous_node.get_parent()
-                previous_node.get_parent().children.append(current_node)
-                current_node.set_parent(previous_node.get_parent())
+                while previous_node.get_depth() != d:
+                    previous_node = previous_node.get_parent()  # Traverse up the tree until reaching the node at depth d
+                previous_node.get_parent().children.append(current_node)  
+                current_node.set_parent(previous_node.get_parent())  
 
-            previous_node = current_node
-            current_depth = d
+            previous_node = current_node  
+            current_depth = d  
         return AST(root)
